b813734e170786d7223d91d608e285bfc9c2a1be
Arshad <arshadrafi566@gmail.com>
2026-01-29 14:59:19 +0530

    fix: Update navbar styles and product logic

--- src/app/ui/product/product.ts
+++ src/app/ui/product/product.ts
@@ -22,9 +22,7 @@ singleProduct: any;
       this.products = data;
     });
   }
-   // {name: 'HARRY', Image: 'https://i.pinimg.com/736x/9c/d6/91/9cd6914fcbe9fbda60d0c83d7dec44f0.jpg'},
-    //{name: 'JEFF', Image: 'https://i.pinimg.com/736x/36/19/d6/3619d62b6082e153293ecf4961745a4e.jpg'}
- // ]
+
 }
 
 
